81e84eb5c1f35ff23cbd96334f7b6ab4394d9071
Chemslayer <nicolinilr@gmail.com>
2020-11-06 09:22:07 -0800

    Complete javascript practice project

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () =>{
+    return Math.floor(Math.random()*10);
+};
+
+const compareGuesses = (humanGuess, compGuess, targetNum) =>{
+    const humanDiff = Math.abs(humanGuess-targetNum);
+    const compDiff = Math.abs(compGuess - targetNum);
+
+    if (compDiff < humanDiff){
+        return false;
+    } else {
+        return true;
+    };
+};
+
+const updateScore = (winner) =>{
+    if (winner === 'human'){
+        humanScore++;
+    } else if (winner === 'computer'){
+        computerScore++;
+    } else {
+        console.log('Something went wrong');
+    };
+};
+
+const advanceRound = () =>{
+    currentRoundNumber++;
+};
+
